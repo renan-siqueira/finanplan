376fbee873acb79a5ec4b352ffeb28981014f227
Renan Siqueira Antonio <renan.siqu@gmail.com>
2022-09-14 20:36:39 -0300

    feat: create NavbarComponent

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,23 +3,31 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataBase } from '../in-memory-database';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataBase)
   ],
   exports: [
+    // shared modules
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule,
+
+    // shared components
+    NavbarComponent
   ]
 })
 export class CoreModule { }
